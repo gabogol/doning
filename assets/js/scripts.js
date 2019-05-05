0f1e4f9335b3aa4c199e3847fbb915eac9d82f0e
Gabriel <gabriel.toro@gmail.com>
2019-05-05 18:41:59 -0400

    cambios 3 parte 1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,18 +2,21 @@ jQuery(document).ready(function() {
 	
 	/*
 	    Navigation
-	*/
+    */
+    /*
 	$('.top-content .text').waypoint(function() {
 		$('nav').toggleClass('navbar-no-bg');
     });
     /*
     $('.top-content .text').waypoint(function() {
 		$('.navbar-brand').toggleClass('navbar-brand2');
-	});
+    });
+    
+    
 	
     /*
         Background slideshow
-    */
+   
     $('.top-content').backstretch("assets/img/backgrounds/1.jpg");
     
     $('#top-navbar-1').on('shown.bs.collapse', function(){
@@ -21,13 +24,45 @@ jQuery(document).ready(function() {
     });
     $('#top-navbar-1').on('hidden.bs.collapse', function(){
     	$('.top-content').backstretch("resize");
+    }); */
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll >= 400) {
+            $(".navbar").removeClass("navbar-no-bg");
+        } else {
+            $(".navbar").addClass("navbar-no-bg");
+        }
     });
 
-   
+    /* Menu Interno */
+
+    $('#openNav, .closebtn').click(function () {
+        $('#sideNav').toggleClass("ancho");
+    });
+
+    /* Tooltip*/
+
+    $('#fechaVenc [data-toggle="tooltip"]').tooltip({
+        animated: 'fade',
+        placement: 'right',
+        html: true
+    });
     
     /*
         Wow
     */
     new WOW().init();
+
+    /*function openNav() {
+        var element = document.getElementById("sideNav");
+        element.classList.toggle("ancho");;
+    }
+    function closeNav() {
+        var element = document.getElementById("sideNav");
+        element.classList.toggle("ancho");;
+    }*/
+    
 	
 });
